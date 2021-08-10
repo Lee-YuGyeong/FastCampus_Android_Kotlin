1978f578619692c93c417f39f5d2d266ed46610b
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2021-08-10 17:08:55 +0900

    타이머 기능 추가

---
+++ PomodoroTimer/src/main/java/com/example/fastcampus_android_kotlin/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.fastcampus_android_kotlin
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.media.SoundPool
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.*
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val remainSecondsTextView: TextView by lazy {
+        findViewById(R.id.remainSecondsTextView)
+    }
+
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
+    private val soundPool = SoundPool.Builder().build()
+
+    private var currentCountDownTimer: CountDownTimer? = null
+
+    private var tickingSoundId: Int? = null
+
+    private var bellSoundId: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindViews()
+        initSounds()
+    }
+
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        updateRemainTime(progress * 60 * 1000L)
+                    }
+
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    currentCountDownTimer?.cancel()
+                    currentCountDownTimer = null
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    seekBar ?: return
+
+                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+                    currentCountDownTimer?.start()
+
+                    tickingSoundId?.let { soundId ->
+                        soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+                    }
+                }
+            }
+        )
+    }
+
+    private fun initSounds() {
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
+    }
+
+    private fun createCountDownTimer(initialMillis: Long) =
+        object : CountDownTimer(initialMillis, 1000L) {
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                updateRemainTime(0)
+                updateSeekBar(0)
+            }
+        }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateRemainTime(remainMillis: Long) {
+
+        val remainSeconds = remainMillis / 1000
+
+        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long) {
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
+    }
+
+}
