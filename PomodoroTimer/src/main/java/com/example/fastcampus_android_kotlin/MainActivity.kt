80d1e01236af124ce4feff14801bb332ef44eac0
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2021-08-10 17:50:47 +0900

    완성도 높이기

--- PomodoroTimer/src/main/java/com/example/fastcampus_android_kotlin/MainActivity.kt
+++ PomodoroTimer/src/main/java/com/example/fastcampus_android_kotlin/MainActivity.kt
@@ -70,14 +70,18 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                    currentCountDownTimer?.cancel()
-                    currentCountDownTimer = null
+                    stopCountDown()
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
                     seekBar ?: return
 
-                    startCountDown()
+                    if (seekBar.progress == 0) {
+                        stopCountDown()
+                    } else {
+                        startCountDown()
+                    }
+
                 }
             }
         )
@@ -109,6 +113,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun stopCountDown() {
+        currentCountDownTimer?.cancel()
+        currentCountDownTimer = null
+        soundPool.autoPause()
+    }
+
     private fun completeCountDown() {
         updateRemainTime(0)
         updateSeekBar(0)
@@ -126,7 +136,7 @@ class MainActivity : AppCompatActivity() {
 
         val remainSeconds = remainMillis / 1000
 
-        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainMinutesTextView.text = "%02d'".format(remainSeconds / 60)
         remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
     }
 
